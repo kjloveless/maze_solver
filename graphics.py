69f0079bc18e7971a65397c0ec3a7464219cec12
Kyle Loveless <kjloveless321@gmail.com>
2024-04-10 13:39:07 -0500

    Draw line on canvas

--- graphics.py
+++ graphics.py
@@ -13,6 +13,9 @@ class Window():
     self.root.update_idletasks()
     self.root.update()
 
+  def draw_line(self, line, fill_color):
+    line.draw(self.canvas, fill_color)
+
   def wait_for_close(self):
     self.running = True
     while (self.running):
@@ -20,3 +23,19 @@ class Window():
 
   def close(self):
     self.running = False
+
+class Point():
+  def __init__(self, x=0, y=0):
+    self.x = x
+    self.y = y
+
+class Line():
+  def __init__(self, p1, p2):
+    self.p1 = p1
+    self.p2 = p2
+
+  def draw(self, canvas, fill_color):
+    canvas.create_line(
+        self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+    )
+    canvas.pack(fill=BOTH, expand=1)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
   win = Window(800, 600)
+  p1 = Point(10, 50)
+  p2 = Point(75, 200)
+  l = Line(p1, p2)
+
+  win.draw_line(l, "red")
+
   win.wait_for_close()
 
 main()
