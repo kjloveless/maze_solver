a332636036e49db7fba7cd07901f8d3a44e8c025
Kyle Loveless <kjloveless321@gmail.com>
2024-04-12 15:57:13 -0500

    Solve maze recursively

--- maze.py
+++ maze.py
@@ -100,3 +100,47 @@ class Maze:
     for col in self.cells:
       for cell in col:
         cell.visited = False
+
+  def solve(self):
+    return self._solve_r(0,0)
+
+  def _solve_r(self, i, j):
+    self._animate()
+    self.cells[i][j].visited = True
+
+    if i == self.num_cols - 1 and j == self.num_rows - 1:
+      return True
+
+    if (i > 0 and not self.cells[i][j].has_left_wall
+        and not self.cells[i - 1][j].visited):
+      self.cells[i][j].draw_move(self.cells[i - 1][j])
+      if self._solve_r(i - 1, j):
+        return True
+      else:
+        self.cells[i][j].draw_move(self.cells[i - 1][j], True)
+
+    if (i < self.num_cols - 1 and not self.cells[i][j].has_right_wall
+        and not self.cells[i + 1][j].visited):
+      self.cells[i][j].draw_move(self.cells[i + 1][j])
+      if self._solve_r(i + 1, j):
+        return True
+      else:
+        self.cells[i][j].draw_move(self.cells[i + 1][j], True)
+
+    if (j > 0 and not self.cells[i][j].has_top_wall
+        and not self.cells[i][j - 1].visited):
+      self.cells[i][j].draw_move(self.cells[i][j - 1])
+      if self._solve_r(i, j - 1):
+        return True
+      else:
+        self.cells[i][j].draw_move(self.cells[i][j - 1], True)
+
+    if (j < self.num_rows - 1 and not self.cells[i][j].has_bottom_wall
+        and not self.cells[i][j + 1].visited):
+      self.cells[i][j].draw_move(self.cells[i][j + 1])
+      if self._solve_r(i, j + 1):
+        return True
+      else:
+        self.cells[i][j].draw_move(self.cells[i][j + 1], True)
+
+    return False
